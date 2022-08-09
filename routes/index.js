2d92e8b1e3cb219acbc1b873b3210f3d5abebf9a
NTA2K <theanhqm@gmail.com>
2022-08-09 13:38:12 +0700

    change

--- routes/index.js
+++ routes/index.js
@@ -11,38 +11,42 @@ const uri = "mongodb+srv://user:user@cluster0.flsq9.mongodb.net/Demo6?retryWrite
 mongoose.connect(uri).catch(err => console.log('co loi xay ra'));
 
 //dinh nghia schema (kieu du lieu) cua model products
-const PRODUCT = mongoose.model('Products',new Schema({
-  name:String,
-  price:Number
+const IMAGE = mongoose.model('wallpapers',new Schema({
+  link:String,
+    description:String,
+  date:Date
 }));
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-PRODUCT.find({},function (error, result){
+IMAGE.find({},function (error, result){
     if (error) throw error;
     console.log(result.length)
-    res.render('index',{title:'Express',data:result})
+     res.render('index',{title:'Express',data:result})
+   // res.send(result);
 })
 });
 
 router.post('/add' , async  function (req,res) {
 //lay cac tham so
-  const  name = req.body.name;
-  const  price = req.body.price;
-  const Products = new PRODUCT({
-      name : name,
-        price:price
+  const  link = req.body.link;
+  const  description = req.body.description;
+  const date = req.body.date;
+  const image = new IMAGE({
+      link : link,
+        description:description,
+      date:date
 
   })
-    await Products.save();
+    await image.save();
   res.redirect('/')
 
 })
 
-router.get('/getUser',function (req,res){
-    const prdList = mongoose.model('Products',);
-    prdList.find({},function (error, result){
+router.get('/getImage',function (req,res){
+    const imgList = mongoose.model('wallpapers');
+    imgList.find({},function (error, result){
         res.send(result)
     })
 
@@ -50,7 +54,7 @@ router.get('/getUser',function (req,res){
 
 router.get('/delete',async  function (req,res){
     const id = req.query.id;
-   PRODUCT.deleteOne({_id:id},function (error){
+   IMAGE.deleteOne({_id:id},function (error){
        if (error) throw error;
    })
     res.redirect('/')
@@ -58,20 +62,23 @@ router.get('/delete',async  function (req,res){
 router.get('/updateForm/', function (req, res) {
 
     const id = req.query.id;
-    PRODUCT.findOne({_id: id}, function (error, result) {
+    IMAGE.findOne({_id: id}, function (error, result) {
         res.render('update', {title: 'Update', data: result})
     })
 
 })
 router.post('/update', async function (req, res) {
     const id =req.body.id;
-    var name  = req.body.name;
-    var  price = req.body.price;
+    var link     = req.body.link;
+    var  description = req.body.description;
+    var date = req.body.date;
 
 
-    await PRODUCT.updateOne({_id: id}, {
-        name: name,
-        price: price
+
+    await IMAGE.updateOne({_id: id}, {
+        link   : link,
+        description: description,
+        date:date
     }, null)
 
     res.redirect('/')
